ab1c7807e6c85dbb303528506ccf1f01e9bc673c
MicrowaveCoffee <32424042+MicrowaveCoffee@users.noreply.github.com>
2025-02-23 21:52:47 -0600

    Game Guessing Functionality Improved

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ const input = document.querySelector('.input');
 output.textContent = 'Press Button to Start'
 btn.textContent = 'Start Game';
 
-const game = {guesses:0, guessValue:0}
+const game = {guesses:0, guessValue:0, randomNum:0};
+
+const random = randomNumber();
+game.randomNum = random;
+
+
 
 btn.addEventListener('click', (e) => {
+    
     
     if(btn.textContent === 'Start Game') {
         output.textContent = 'Guess between 0 - 10';
@@ -16,19 +22,32 @@ btn.addEventListener('click', (e) => {
         input.style.display = 'block';
         
     } else if (btn.textContent === 'Guess Value') {
-       const num = parseInt(input.value);
-       console.log(typeof(num));
-    }
-    
-    
-    // const returnedRandomValue = randomNumber();
+       const num = Number(input.value);
+       game.guessValue = num;
+       if(!num) {
+        Swal.fire('Please Type In A Number Value')
+       } return compareGuess()
+    };
     
     
     
 })
 
 function compareGuess() {
-
+    
+    if(game.guessValue === game.randomNum) {
+        input.value = ''
+        Swal.fire('Congrats You Win')
+        btn.textContent = 'Start'
+        input.style.display = 'none';
+    } else if (game.guessValue > game.randomNum) {
+        Swal.fire('Guess Is Too High')
+        input.value = ''
+    } else if (game.guessValue < game.randomNum) {
+        Swal.fire('Guess Is Too Low')
+        input.value = ''
+    }
+    
 }
 
 function createNewElement(type, parent, className){
@@ -38,8 +57,8 @@ function createNewElement(type, parent, className){
 }
 
 function randomNumber() {
-    const random = Math.floor(Math.random() * 10)
-    return random
+    return Math.floor(Math.random() * 10)
+   
 }
 
 
